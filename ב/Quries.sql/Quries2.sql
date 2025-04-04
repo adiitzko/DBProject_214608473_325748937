da6770c9fa45154fcc0b170d8593025ad100bd70
gilishechter <gishecht@g.jct.ac.il>
2025-04-04 16:29:24 +0300

    update

--- ב/Quries.sql/Quries2.sql
+++ ב/Quries.sql/Quries2.sql
@@ -1,6 +1,5 @@
-//כל המלונות ביעד מסוים 
-SELECT h.hotelID, h.hotelName, h.location, h.totalRooms
+//עבור כל מלון את היעד שבו הוא נמצא
+SELECT h.hotelID, h.hotelName, d.country, d.address
 FROM hotel h
 JOIN trip t ON h.hotelID = t.hotelID
-JOIN destination d ON t.destinationZip = d.zipCode
-WHERE d.country = 'Israel';  
+JOIN destination d ON t.destinationZip = d.zipCode;
