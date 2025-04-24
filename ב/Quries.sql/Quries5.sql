264637174c969b963490a2a424f2d18e165edb50
Adi Itzkovich <154916859+adiitzko@users.noreply.github.com>
2025-04-24 23:45:07 +0300

    Update Quries5.sql

--- ב/Quries.sql/Quries5.sql
+++ ב/Quries.sql/Quries5.sql
@@ -1,14 +1,18 @@
 #image
 //טיסות בין ראשון לחמישי ליעדים שביקרו בהם לפחות 100 לקוחות
-SELECT f.flightID, t.tripID, f.airline, f.departureDate, f.returnDate
+SELECT f.flightID, f.airline, f.departureDate, f.returnDate, t.tripID
 FROM flight f
-JOIN trip t ON f.departureDate = t.startDate AND f.returnDate = t.endDate
+JOIN trip t ON f.departureDate BETWEEN t.startDate AND t.endDate
 WHERE EXTRACT(DOW FROM f.departureDate) BETWEEN 0 AND 4
   AND EXTRACT(DOW FROM f.returnDate) BETWEEN 0 AND 4
-  AND t.destinationZip IN (
-      SELECT destinationZip
-      FROM trip
-      JOIN invite USING(tripID)
-      GROUP BY destinationZip
-      HAVING SUM(totalCustomer) > 100
-  );
+  AND EXISTS (
+      SELECT 1
+      FROM trip t2
+      JOIN invite i ON t2.tripID = i.tripID
+      WHERE t2.destinationZip IN (
+          SELECT destinationZip
+          FROM trip
+          JOIN invite USING(tripID)
+          GROUP BY destinationZip
+          HAVING SUM(totalCustomer) > 100) );
+
