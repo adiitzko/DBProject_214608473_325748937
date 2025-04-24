988609294a65914e5ba01c0514e7f2b63989210a
gili shechter <148372194+gilishechter@users.noreply.github.com>
2025-04-24 18:58:05 +0300

    Update Quries4.sql

--- ב/Quries.sql/Quries4.sql
+++ ב/Quries.sql/Quries4.sql
@@ -1,7 +1,8 @@
+#TO DO
 //רשימת כל הטיסות ופרטי היעד שלהם 
 SELECT f.airline, f.departureDate, f.returnDate, d.country, h.hotelName, t.startDate, t.endDate
 FROM flight f
-JOIN include inc ON f.flightID = inc.flightID
+--JOIN include inc ON f.flightID = inc.flightID
 JOIN trip t ON inc.tripID = t.tripID
 JOIN destination d ON t.destinationZip = d.zipCode
 JOIN hotel h ON t.hotelID = h.hotelID;
