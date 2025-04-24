918a59e094c923f62ebfd8e601bdd4a4c0d97eb3
Adi Itzkovich <154916859+adiitzko@users.noreply.github.com>
2025-04-25 00:17:08 +0300

    Update Quries6.sql

--- ב/Quries.sql/Quries6.sql
+++ ב/Quries.sql/Quries6.sql
@@ -1,7 +1,16 @@
-// הלקוחות שהשתתפו בטיולים בעונת הקיץ (יוני-אוגוסט)
-
-SELECT c.fullName, c.phoneNumber, t.tripID, t.startDate, t.endDate
-FROM customer c
-JOIN invite i ON c.ID = i.customerID
-JOIN trip t ON i.tripID = t.tripID
-WHERE EXTRACT(MONTH FROM t.startDate) IN (6, 7, 8);
+#image
+// מדריכים שפנוים באוגוסט והמדינות שבהן הם הדריכו בשאר השנה
+SELECT g.guideID, g.guideName, g.phoneNumber,
+    (SELECT string_agg(DISTINCT d.country, ', ')
+     FROM trip t
+     JOIN destination d ON t.destinationZip = d.zipCode
+     WHERE t.guideID = g.guideID AND EXTRACT(MONTH FROM t.startDate) <> 8) 
+	 AS other_countries_guided
+FROM guide g
+LEFT JOIN trip t_aug ON g.guideID = t_aug.guideID AND 
+EXTRACT(MONTH FROM t_aug.startDate) = 8
+WHERE t_aug.tripID IS NULL
+  AND EXISTS (SELECT 1
+              FROM trip t_other
+              WHERE t_other.guideID = g.guideID
+                AND EXTRACT(MONTH FROM t_other.startDate) <> 8);
