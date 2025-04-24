5622b4f8121d0cf232a8e20acc0d2f490a86c158
Adi Itzkovich <154916859+adiitzko@users.noreply.github.com>
2025-04-25 00:26:36 +0300

    Update Quries7.sql

--- ב/Quries.sql/Quries7.sql
+++ ב/Quries.sql/Quries7.sql
@@ -1,8 +1,9 @@
-//בתי מלון עם תפוסה מלאה
-
+#image
+//בתי מלון שהתפוסה שלהם לא היתה מלאה ברבעון הראשון של 2024
 SELECT DISTINCT h.hotelName, h.totalRooms
 FROM hotel h
 JOIN trip t ON h.hotelID = t.hotelID
 JOIN invite i ON t.tripID = i.tripID
+WHERE h.hotelID IN (SELECT hotelID FROM trip WHERE startDate >= '2024-01-01' AND endDate <= '2024-03-31') 
 GROUP BY h.hotelName, h.totalRooms
-HAVING SUM(i.totalCustomer) >= h.totalRooms * 2;
+HAVING SUM(i.totalCustomer) <= h.totalRooms * 2;
