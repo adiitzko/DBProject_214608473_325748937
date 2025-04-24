494607bc486ebc88d65611db3741a7ad63754f3a
Adi Itzkovich <154916859+adiitzko@users.noreply.github.com>
2025-04-24 22:36:46 +0300

    Update Quries2.sql

--- ב/Quries.sql/Quries2.sql
+++ ב/Quries.sql/Quries2.sql
@@ -1,6 +1,14 @@
-#TO DO
-//עבור כל מלון את היעד שבו הוא נמצא
-SELECT h.hotelID, h.hotelName, d.country, d.address
-FROM hotel h
-JOIN trip t ON h.hotelID = t.hotelID
-JOIN destination d ON t.destinationZip = d.zipCode;
+#image
+//היעד או היעדים עם הכי הרבה לקוחות
+WITH destination_totals AS (
+    SELECT d.zipCode, d.country, d.address, SUM(i.totalCustomer) AS total_customers
+    FROM destination d
+    JOIN trip t ON d.zipCode = t.destinationZip
+    JOIN invite i ON t.tripID = i.tripID
+    GROUP BY d.zipCode, d.country, d.address
+)
+SELECT *
+FROM destination_totals
+WHERE total_customers = (
+    SELECT MAX(total_customers) FROM destination_totals
+);
