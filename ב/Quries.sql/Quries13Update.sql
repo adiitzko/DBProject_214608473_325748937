30a9d4266af9eb57e9109b31817b179ed07c46a9
gili shechter <148372194+gilishechter@users.noreply.github.com>
2025-04-24 19:12:54 +0300

    Update Quries13Update.sql

--- ב/Quries.sql/Quries13Update.sql
+++ ב/Quries.sql/Quries13Update.sql
@@ -1,4 +1,5 @@
-// שינוי שם חברות תעופה למבצעי קיץ
+#TO DO
+  // שינוי שם חברות תעופה למבצעי קיץ
 UPDATE flight 
 SET airline = CONCAT(airline, ' - Summer Promo')
 WHERE EXTRACT(MONTH FROM departureDate) IN (6, 7, 8);
