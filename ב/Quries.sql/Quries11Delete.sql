a113ef9b29ad5a70dbc2b6294ebf91919faee9d4
gili shechter <148372194+gilishechter@users.noreply.github.com>
2025-04-25 10:00:47 +0300

    Update Quries11Delete.sql

--- ב/Quries.sql/Quries11Delete.sql
+++ ב/Quries.sql/Quries11Delete.sql
@@ -1,7 +1,6 @@
-#TO DO
-//מחיקת טיולים שלא התקבלו בהם הזמנות (ללא לקוחות)
+//השאילתא מחפשת טיולים שאין להם לקוחות מוזמנים ושכבר עברו את תאריך ההתחלה, ומציגה את כל הנתונים עליהם.
 DELETE FROM trip
 WHERE tripID NOT IN (
-    SELECT DISTINCT tripID
-    FROM invite
-);
+    SELECT DISTINCT tripID FROM invite
+)
+AND startDate < CURRENT_DATE;
