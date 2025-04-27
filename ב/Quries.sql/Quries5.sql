e3dbf8136819a6961d71f3dbf17ab6f665dc8097
Adi Itzkovich <154916859+adiitzko@users.noreply.github.com>
2025-04-27 22:18:04 +0300

    Update Quries5.sql

--- ב/Quries.sql/Quries5.sql
+++ ב/Quries.sql/Quries5.sql
@@ -1,10 +1,12 @@
 //יעד עם כמות הלקוחות המינימלית ביותר
-SELECT destinationZip, country
-FROM destination
-WHERE zipCode IN (
-    SELECT destinationZip
-    FROM trip
-    JOIN invite USING(tripID)
-    GROUP BY destinationZip
-    HAVING COUNT(DISTINCT customerID) < 10
+SELECT d.zipCode, d.country, t.tripID
+FROM destination d
+JOIN trip t ON t.destinationZip = d.zipCode
+WHERE d.zipCode IN (
+    SELECT t2.destinationZip
+    FROM trip t2
+    JOIN invite i ON t2.tripID = i.tripID
+    GROUP BY t2.destinationZip
+    HAVING COUNT(DISTINCT i.customerID) < 10
 );
+
