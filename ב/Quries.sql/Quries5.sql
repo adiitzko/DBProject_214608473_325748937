3847949f812cc74cc4846f768c407a646667d305
Adi Itzkovich <154916859+adiitzko@users.noreply.github.com>
2025-04-27 22:15:07 +0300

    Update Quries5.sql

--- ב/Quries.sql/Quries5.sql
+++ ב/Quries.sql/Quries5.sql
@@ -1,15 +1,10 @@
-//טיסות בין ראשון לחמישי ליעדים שביקרו בהם פחות מ 100 לקוחות
-SELECT f.flightID, f.airline, f.departureDate, f.returnDate,
-       d.country
-FROM flight f
-JOIN trip t ON f.flightID = t.flightID
-JOIN destination d ON t.destinationZip = d.zipCode
-WHERE EXTRACT(DOW FROM f.departureDate) BETWEEN 0 AND 4
-  AND EXTRACT(DOW FROM f.returnDate) BETWEEN 0 AND 4
-  AND d.zipCode IN (
+//יעד עם כמות הלקוחות המינימלית ביותר
+SELECT destinationZip, country
+FROM destination
+WHERE zipCode IN (
     SELECT destinationZip
     FROM trip
     JOIN invite USING(tripID)
     GROUP BY destinationZip
-    HAVING SUM(totalCustomer) < 100
-  );
+    HAVING COUNT(DISTINCT customerID) < 10
+);
